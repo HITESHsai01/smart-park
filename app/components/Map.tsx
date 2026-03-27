023b9341b9287aab51a850f10ba8bae76edc7194
akash s <ak05ash20@gmail.com>
2026-03-27 18:16:10 +0530

    added style

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -104,10 +104,9 @@ export default function Map({
       scrollWheelZoom={true}
       className="h-full w-full z-0"
     >
-      assName="h-full w-full z-0"
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+        url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
       />
       <MapContent
         properties={properties}
